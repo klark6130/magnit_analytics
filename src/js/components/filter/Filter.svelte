21b93280cf476840c4f5f3291549024f63b443f7
Ilja Biryukov <uncle.funkay@gmail.com>
2020-08-26 09:28:40 +0300

    add filter scroll

--- src/js/components/filter/Filter.svelte
+++ src/js/components/filter/Filter.svelte
@@ -2,11 +2,33 @@
   import FilterGroup from './FilterGroup.svelte';
 
   export let config;
+
+  let filter;
+
+  function shouldScroll(deltaX, deltaY) {
+    return Math.abs(deltaX) > 0 && Math.abs(deltaX) > Math.abs(deltaY);
+  }
+
+  function scrollFilter(deltaX) {
+    filter.scrollLeft += deltaX;
+  }
+
+  function onWheel(event) {
+    const { deltaX, deltaY } = event;
+    if (shouldScroll(deltaX, deltaY)) {
+      event.preventDefault();
+      event.stopPropagation();
+      scrollFilter(deltaX);
+    }
+  }
 </script>
 
-<div class="txcm-filter">
-  {#each config as group}
-    <FilterGroup
-      {group} />
-  {/each}
+<div
+  class="txcm-filter"
+  bind:this={filter}
+  on:wheel={onWheel}>
+    {#each config as group}
+      <FilterGroup
+        {group} />
+    {/each}
 </div>
